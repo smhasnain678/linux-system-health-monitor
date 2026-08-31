6c06c5f2af73c45ba15cbc3defb82e7b7ac866f9
smhasnain678 <smhasnain43@gmail.com>
2026-08-31 20:04:57 +0500

    Project complete

--- health_monitor.sh
+++ health_monitor.sh
@@ -1,10 +1,37 @@
 #!/bin/bash
 
 
-echo "==============================="
+echo "=============================="
 echo "    SYSTEM HEALTH MONITOR"
-echo "==============================="
+echo "=============================="
 
+# Basic System Information
 echo "Hostname   : $(hostname)"
 echo "Date       : $(date)"
+echo "Uptime	   : $(uptime -p)"
+
+echo "=============================="
+
+# CPU Usage
+echo "CPU Usage:" 
+top -bn1 | grep "Cpu(s)" | awk '{print 100 - $8 "%"}'
+
+echo "=============================="
+
+# Memory Usage
+echo "Memory Usage:" 
+free -h
+
+echo "=============================="
+
+# Disk Usage
+echo "Disk Usage:"
+df -h /
+
+echo "=============================="
+
+# Logged-in Users
+echo "Logged-in Users:"
+w
+
 echo "=============================="
